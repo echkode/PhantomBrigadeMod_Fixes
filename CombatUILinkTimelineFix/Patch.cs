28823d866b3af38958c525cc432c103f5781dd6f
EchKode <48565771+echkode@users.noreply.github.com>
2023-12-07 00:38:37 +0000

    Prevent autogenerated runs from showing in timeline

---
+++ CombatUILinkTimelineFix/Patch.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2023 EchKode
+// SPDX-License-Identifier: BSD-3-Clause
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+using HarmonyLib;
+
+using PBCombatUILinkTimeline = PhantomBrigade.Combat.Systems.CombatUILinkTimeline;
+
+namespace EchKode.PBMods.CombatUILinkTimelineFix
+{
+	[HarmonyPatch]
+	static class Patch
+	{
+		[HarmonyPatch(typeof(PBCombatUILinkTimeline), "Execute")]
+		[HarmonyTranspiler]
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+			var cm = new CodeMatcher(instructions, generator);
+			var hasDurationPropInfo = AccessTools.DeclaredPropertyGetter(typeof(ActionEntity), nameof(ActionEntity.hasDuration));
+			var isExtrapolatedPropInfo = AccessTools.DeclaredPropertyGetter(typeof(ActionEntity), nameof(ActionEntity.isMovementExtrapolated));
+			var hasDurationMatch = new CodeMatch(OpCodes.Callvirt, hasDurationPropInfo);
+			var isExtrapolated = new CodeInstruction(OpCodes.Callvirt, isExtrapolatedPropInfo);
+
+			cm.MatchStartForward(hasDurationMatch)
+				.Advance(-1);
+			var loadEntity = new CodeInstruction(cm.Instruction);
+			cm.Advance(2);
+			var branch = new CodeInstruction(OpCodes.Brtrue, cm.Operand);
+			cm.Advance(1)
+				.InsertAndAdvance(loadEntity)
+				.InsertAndAdvance(isExtrapolated)
+				.InsertAndAdvance(branch);
+
+
+			return cm.InstructionEnumeration();
+		}
+	}
+}

ec7875e47fcb81854c3819a1456e33a4d18941ed
EchKode <48565771+echkode@users.noreply.github.com>
2024-01-24 22:43:35 +0000

    Add better comments to DataContainerPartPreset patches so it's clearer about what's being patched

--- DataContainerPartPresetFix/Patch.cs
+++ DataContainerPartPresetFix/Patch.cs
@@ -14,10 +14,19 @@ namespace EchKode.PBMods.DataContainerPartPresetFix
 	[HarmonyPatch]
 	static class Patch
 	{
-		[HarmonyPatch(typeof(PBDataContainerPartPreset), "CompareGenStepsForSorting", new System.Type[] { typeof(IPartGenStep), typeof(IPartGenStep) })]
-		[HarmonyTranspiler]
-		static IEnumerable<CodeInstruction> CompareGenStepsForSorting_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		[HarmonyPatch(typeof(PBDataContainerPartPreset), "CompareGenStepsForSorting", new System.Type[]
 		{
+			typeof(IPartGenStep),
+			typeof(IPartGenStep)
+		})]
+		[HarmonyTranspiler]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Used by the Harmony patch system")]
+		static IEnumerable<CodeInstruction> Dcpp_CompareGenStepsForSortingTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+			// Change the return values when step1 or step2 is null so that nulls sort to the end.
+			// When step1 is null and step2 is non-null, the function should return 1 so that step1 is sorted after step2.
+			// When step2 is null and step1 is non-null, the function should return -1 so that step1 is sorted before step2.
+
 			var cm = new CodeMatcher(instructions, generator);
 			var negOneMatch = new CodeMatch(OpCodes.Ldc_I4_M1);
 			var oneMatch = new CodeMatch(OpCodes.Ldc_I4_1);
@@ -31,17 +40,23 @@ namespace EchKode.PBMods.DataContainerPartPresetFix
 		}
 
 		[HarmonyPatch(typeof(PBDataContainerPartPreset), "SortGenSteps", new System.Type[] { typeof(List<IPartGenStep>) })]
-		[HarmonyPostfix]
-		static void Dcpp_SortGenStepsPostfix(List<IPartGenStep> steps)
+		[HarmonyTranspiler]
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Used by the Harmony patch system")]
+		static IEnumerable<CodeInstruction> Dcpp_SortGenStepTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
 		{
-			if (steps == null)
-			{
-				return;
-			}
-			while (steps.Count != 0 && steps[steps.Count - 1] == null)
-			{
-				steps.RemoveAt(steps.Count - 1);
-			}
+			// Exit the loop once we reach a non-null value. Since the list is sorted with nulls at the end, we don't
+			// have to go any farther once we reach a non-null value.
+
+			var cm = new CodeMatcher(instructions, generator);
+			var branchMatch = new CodeMatch(OpCodes.Brtrue_S);
+
+			cm.End();
+			cm.CreateLabel(out var retLabel);
+
+			cm.MatchStartBackwards(branchMatch)
+				.SetOperandAndAdvance(retLabel);
+
+			return cm.InstructionEnumeration();
 		}
 	}
 }
